8e269a7f7f2fb0411db8c2f1cf07752448c255c5
Leung-Kam-Ho <kamho.leung@polyu.edu.hk>
2026-05-29 11:18:00 +0800

    Remove unused Fast DDS configuration files and scripts; add static discovery profile configuration

--- local_ds_PI.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=/home/dev/Mac-ROS2-fastDDS/fastdds_tunnel.xml
-export ROS_AUTOMATIC_DISCOVERY_RANGE=OFF
-export ROS_DISCOVERY_SERVER="127.0.0.1:11811"
-echo "Pi Environment Configured!"
