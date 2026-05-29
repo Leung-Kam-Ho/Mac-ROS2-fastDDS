9a62234789ce4d3c036365b216154ea5aed9ebff
Leung-Kam-Ho <kamho.leung@polyu.edu.hk>
2026-05-29 11:19:33 +0800

    Add initial setup scripts for Fast DDS configuration

---
+++ local_dds.sh
@@ -0,0 +1,3 @@
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export FASTRTPS_DEFAULT_PROFILES_FILE=~/.fastdds_profiles.xml
+source /opt/ros/jazzy/setup.bash 
---
+++ local_dds_PI.sh
@@ -0,0 +1,2 @@
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+source /opt/ros/jazzy/setup.bash
